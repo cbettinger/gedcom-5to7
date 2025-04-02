c2007808eb3fa1b4630e7d34f8a30218ea1633f3
Christian Bettinger <c.bettinger@hochschule-trier.de>
2025-04-02 10:46:28 +0200

    Improved GedStruct

--- src/main/java/bettinger/gedcom5to7/GedStruct.java
+++ src/main/java/bettinger/gedcom5to7/GedStruct.java
@@ -2,227 +2,302 @@ package bettinger.gedcom5to7;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GedStruct {
-    public GedStruct sup;
-    public LinkedList<GedStruct> sub;
-    int level;
-    public String tag, id, payload;
-    public String uri;
-    public Collection<GedStruct> incoming;
-    public GedStruct pointsTo;
-    final static Pattern GED_LINE = Pattern.compile("﻿?\\s*([0-9]+)\\s+(?:(@[^@]+@)\\s+)?([A-Za-z0-9_]+)(?:\\s([^\\n\\r]*))?[\\n\\r]*");
-    final static Pattern FUZZY_XREF = Pattern.compile("\\s*@[^#@][^@]*@\\s*");
-    final static Pattern AT_DETECTOR = Pattern.compile(
-        "^\\s*(@[^@#][^@]*@)\\s*$|" +
-        "(@@)|" +
-        "@#D([^@]*)@ ?|" +
-        "@#([^D@][^@]*)@ ?"
-        );
-    public final static GedStruct VOID;
-    static {
-        VOID = new GedStruct(null, "");
-        VOID.id = "@VOID@";
-    }
+	public GedStruct sup;
+	public List<GedStruct> sub;
+	public int level;
+	public String tag;
+	public String id;
+	public String payload;
+	public String uri;
+	public Collection<GedStruct> incoming;
+	public GedStruct pointsTo;
 
-    /**
-     * Parse a line from a GEDCOM file into a basic GedStruct
-     *
-     * @throws@ IllegalArgumentException if the line cannot be parsed
-     */
-    public GedStruct(String line) {
-        Matcher m = GED_LINE.matcher(line);
-        if (!m.matches()) throw new IllegalArgumentException("No GEDCOM found in:\n"+line);
+	private static final Pattern GED_LINE = Pattern.compile("﻿?\\s*([0-9]+)\\s+(?:(@[^@]+@)\\s+)?([A-Za-z0-9_]+)(?:\\s([^\\n\\r]*))?[\\n\\r]*");
+	private static final Pattern FUZZY_XREF = Pattern.compile("\\s*@[^#@][^@]*@\\s*");
+	private static final Pattern AT_DETECTOR = Pattern.compile("^\\s*(@[^@#][^@]*@)\\s*$|" + "(@@)|" + "@#D([^@]*)@ ?|" + "@#([^D@][^@]*)@ ?");
 
-        sub = new LinkedList<GedStruct>();
-        level = Integer.parseInt(m.group(1));
-        if (m.group(2) != null) {
-            id = m.group(2).toUpperCase();
-            if ("@VOID@".equals(id)) throw new IllegalArgumentException("@VOID@ must not be used as a record identifier in:\n"+line);
-        }
-        tag = m.group(3).toUpperCase();
-        payload = fixAtSign(m.group(4));
-    }
+	public static final GedStruct VOID;
 
+	static {
+		VOID = new GedStruct(null, "");
+		VOID.id = "@VOID@";
+	}
 
-    public GedStruct(GedStruct sup, String tag) {
-        this.sub = new LinkedList<GedStruct>();
-        if (tag.indexOf(':') < 0) this.tag = tag;
-        else { this.uri = tag; this.uri2tag(); }
-        if (sup != null) { sup.addSubstructure(this); this.level = sup.level+1; }
-        else { this.sup = null; this.level = 0; }
-    }
-    public GedStruct(GedStruct sup, String tag, String payload) {
-        this.sub = new LinkedList<GedStruct>();
-        if (tag.indexOf(':') < 0) this.tag = tag;
-        else { this.uri = tag; this.uri2tag(); }
-        this.payload = payload;
-        if (sup != null) { sup.addSubstructure(this); this.level = sup.level+1; }
-        else { this.sup = null; this.level = 0; }
-    }
-    public GedStruct(GedStruct sup, String tag, GedStruct payload) {
-        this.sub = new LinkedList<GedStruct>();
-        if (tag.indexOf(':') < 0) this.tag = tag;
-        else { this.uri = tag; this.uri2tag(); }
-        this.pointsTo = payload;
-        if (payload == null) this.pointsTo = VOID;
-        else if (payload.incoming != null) payload.incoming.add(this);
-        else {
-            payload.incoming = new LinkedList<GedStruct>();
-            payload.incoming.add(this);
-        }
-        if (sup != null) { sup.addSubstructure(this); this.level = sup.level+1; }
-        else { this.sup = null; this.level = 0; }
-    }
+	/**
+	 * Parse a line from a GEDCOM file into a basic GedStruct @throws@
+	 * IllegalArgumentException if the line cannot be parsed
+	 */
+	public GedStruct(final String line) {
+		final var matcher = GED_LINE.matcher(line);
+		if (!matcher.matches())
+			throw new IllegalArgumentException("No GEDCOM found in:\n" + line);
 
-    static private String fixAtSign(String payload) {
-        if (payload == null || payload.length() == 0) return null;
-        Matcher m = AT_DETECTOR.matcher(payload);
-        String ans = "";
-        int lastidx = 0;
-        while(m.find()) {
-            if (m.group(1) != null) return m.group(1); // pointer
-            if (m.group(2) != null) { // @@
-                ans += payload.substring(lastidx, m.start()+1);
-            } else if (m.group(3) != null) { // date
-                ans += payload.substring(lastidx, m.start());
-                ans += m.group(3).replaceAll("\\s","_");
-                ans += ' ';
-            } else if (m.group(4) != null) {
-                ans += payload.substring(lastidx, m.start());
-                // skip non-date escape, which have been invalid since 5.3
-            } else {
-                throw new UnsupportedOperationException("Impossible: \""+m.group(0)+"\" matched the AT_DETECTOR");
-            }
-            lastidx = m.end();
-        }
-        ans += payload.substring(lastidx);
-        return ans;
-    }
+		sub = new LinkedList<>();
+		level = Integer.parseInt(matcher.group(1));
+		if (matcher.group(2) != null) {
+			id = matcher.group(2).toUpperCase();
+			if ("@VOID@".equals(id))
+				throw new IllegalArgumentException("@VOID@ must not be used as a record identifier in:\n" + line);
+		}
 
-    public void tag2uri() { this.tag2uri(true); }
-    public void tag2uri(boolean replaceSelf) {
-        GedcomDefinitions def = GedcomDefinitions.get();
-        if (uri == null || replaceSelf) {
-            if (sup == null) uri = def.getStructure("", tag);
-            else if (sup.uri == null || def.getTag(sup.uri) == null)
-                uri = def.getStructure(null, tag);
-            else uri = def.getStructure(sup.uri, tag);
-        }
-        for(GedStruct kid : sub) kid.tag2uri(replaceSelf);
-    }
-    public void uri2tag() {
-        if (uri != null) {
-            String tag2 = GedcomDefinitions.get().getTag(uri);
-            if (tag2 != null) tag = tag2;
-        }
-        for(GedStruct kid : sub) kid.uri2tag();
-    }
+		tag = matcher.group(3).toUpperCase();
+		payload = fixAtSign(matcher.group(4));
+	}
 
-    /**
-     * Adds a substructure to this structure. If the substructure is
-     * a CONT or CONC, this operation modifies the payload; otherwise
-     * it adds bidirectional linking between the structures. Because
-     * substructure order is significant, this method must be called
-     * in the order substructures appear in the file.
-     */
-    public boolean addSubstructure(GedStruct substructure) {
-        if ("CONT".equals(substructure.tag)) {
-            payload += "\n"+substructure.payload;
-            return false;
-        } else if ("CONC".equals(substructure.tag)) {
-            payload += substructure.payload;
-            return false;
-        } else {
-            sub.add(substructure);
-            substructure.sup = this;
-            return true;
-        }
-    }
+	public GedStruct(final GedStruct sup, final String tag) {
+		this.sub = new LinkedList<>();
 
-    /**
-     * Given a map from xref_id strings to their parsed structures,
-     * populates the <code>pointsTo</code> fields of this struct
-     * and its substructures. If <code>cleanup</code> is
-     * <code>true</code>, also clears old xref_id payloads and
-     * replaces dead pointers with <code"@VOID@"</code>.
-     */
-    @SuppressWarnings({"unchecked"})
-    public void convertPointers(Map<String,GedStruct> xref, boolean cleanup, Class<?> cachetype) {
-        if (payload != null) {
-            GedStruct to = xref.get(this.payload);
-            if (to != null) {
-                if (cachetype != null) {
-                    try {
-                        if (to.incoming == null) to.incoming = (Collection<GedStruct>)cachetype.getConstructor().newInstance();
-                        to.incoming.add(this);
-                    } catch (NoSuchMethodException ex) {}
-                    catch (InstantiationException ex) {}
-                    catch (IllegalAccessException ex) {}
-                    catch (java.lang.reflect.InvocationTargetException ex) {}
-                }
-                pointsTo = to;
-                if (cleanup) payload = null;
-            } else if (cleanup && FUZZY_XREF.matcher(payload).matches()) {
-                pointsTo = VOID;
-                // TO DO: log this workaround
-            }
-        }
-        for(GedStruct s : sub) s.convertPointers(xref, cleanup, cachetype);
-    }
+		if (tag.indexOf(':') < 0)
+			this.tag = tag;
+		else {
+			this.uri = tag;
+			this.uri2tag();
+		}
 
-    public void pointTo(GedStruct struct) {
-        if (pointsTo == struct) return;
-        if (pointsTo != null && pointsTo.incoming != null) pointsTo.incoming.remove(this);
-        pointsTo = struct;
-        if (pointsTo != null) {
-            if (pointsTo.incoming == null) pointsTo.incoming = new LinkedList<GedStruct>();
-            pointsTo.incoming.add(this);
-        } else {
-            pointsTo = VOID;
-        }
-    }
+		if (sup != null) {
+			sup.addSubstructure(this);
+			this.level = sup.level + 1;
+		} else {
+			this.sup = null;
+			this.level = 0;
+		}
+	}
 
-    // validate payload datatypes and pointed-to types
+	public GedStruct(final GedStruct sup, final String tag, final String payload) {
+		this.sub = new LinkedList<>();
 
-    /**
-     * Helper method for toString
-     */
-    void serialize(StringBuilder sb) {
-        if (sup != null) level = sup.level + 1;
-        sb.append(level); sb.append(' ');
-        // to do: add ID addition if no ID but pointed to
-        if (level == 0 && id != null) {
-            sb.append(id);
-            sb.append(' ');
-        }
-        if (uri == null && !"CONT".equals(tag) && !"TRLR".equals(tag)) sb.append("_EXT_");
-        sb.append(tag);
-        if (pointsTo != null) {
-            // to do: add ID addition if point to non-ID struct
-            sb.append(' ');
-            sb.append(pointsTo.id);
-        }
-        else if (payload != null) {
-            sb.append(' ');
-            sb.append(payload.replaceAll("^@|\\n@|\\r@","$0@").replaceAll("\r\n?|\n", "\n"+(level+1)+" CONT "));
-        }
-        //if (uri != null) sb.append("    <"+uri+">");
-        sb.append("\n");
-        for(GedStruct s : sub) s.serialize(sb);
-    }
+		if (tag.indexOf(':') < 0)
+			this.tag = tag;
+		else {
+			this.uri = tag;
+			this.uri2tag();
+		}
 
-    /**
-     * Serializes as GEDCOM. Advanced data overrides primitive data:
-     * for example, superstructure supersedes level, pointers
-     * supersede payloads, etc.
-     */
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        serialize(sb);
-        return sb.toString();
-    }
+		this.payload = payload;
+
+		if (sup != null) {
+			sup.addSubstructure(this);
+			this.level = sup.level + 1;
+		} else {
+			this.sup = null;
+			this.level = 0;
+		}
+	}
+
+	public GedStruct(final GedStruct sup, final String tag, final GedStruct payload) {
+		this.sub = new LinkedList<>();
+
+		if (tag.indexOf(':') < 0)
+			this.tag = tag;
+		else {
+			this.uri = tag;
+			this.uri2tag();
+		}
+
+		this.pointsTo = payload;
+
+		if (payload == null)
+			this.pointsTo = VOID;
+		else if (payload.incoming != null)
+			payload.incoming.add(this);
+		else {
+			payload.incoming = new LinkedList<>();
+			payload.incoming.add(this);
+		}
+
+		if (sup != null) {
+			sup.addSubstructure(this);
+			this.level = sup.level + 1;
+		} else {
+			this.sup = null;
+			this.level = 0;
+		}
+	}
+
+	private static String fixAtSign(final String payload) {
+		if (payload == null || payload.isEmpty())
+			return null;
+
+		String result = "";
+
+		final var matcher = AT_DETECTOR.matcher(payload);
+		int lastidx = 0;
+		while (matcher.find()) {
+			if (matcher.group(1) != null)
+				return matcher.group(1); // pointer
+			if (matcher.group(2) != null) { // @@
+				result += payload.substring(lastidx, matcher.start() + 1);
+			} else if (matcher.group(3) != null) { // date
+				result += payload.substring(lastidx, matcher.start());
+				result += matcher.group(3).replaceAll("\\s", "_");
+				result += ' ';
+			} else if (matcher.group(4) != null) {
+				result += payload.substring(lastidx, matcher.start());
+				// skip non-date escape, which have been invalid since 5.3
+			} else {
+				throw new UnsupportedOperationException("Impossible: \"" + matcher.group(0) + "\" matched the AT_DETECTOR");
+			}
+
+			lastidx = matcher.end();
+		}
+
+		result += payload.substring(lastidx);
+
+		return result;
+	}
+
+	public void tag2uri() {
+		tag2uri(true);
+	}
+
+	public void tag2uri(final boolean replaceSelf) {
+		final var definitions = GedcomDefinitions.get();
+		if (uri == null || replaceSelf) {
+			if (sup == null)
+				uri = definitions.getStructure("", tag);
+			else if (sup.uri == null || definitions.getTag(sup.uri) == null)
+				uri = definitions.getStructure(null, tag);
+			else
+				uri = definitions.getStructure(sup.uri, tag);
+		}
+
+		for (final var subStructure : sub)
+			subStructure.tag2uri(replaceSelf);
+	}
+
+	public void uri2tag() {
+		if (uri != null) {
+			final var tag2 = GedcomDefinitions.get().getTag(uri);
+			if (tag2 != null)
+				tag = tag2;
+		}
+
+		for (final var subStruct : sub)
+			subStruct.uri2tag();
+	}
+
+	/**
+	 * Adds a substructure to this structure. If the substructure is a CONT or CONC,
+	 * this operation modifies the payload; otherwise it adds bidirectional linking
+	 * between the structures. Because substructure order is significant, this
+	 * method must be called in the order substructures appear in the file.
+	 */
+	public boolean addSubstructure(final GedStruct substructure) {
+		if ("CONT".equals(substructure.tag)) {
+			payload += "\n" + substructure.payload;
+			return false;
+		} else if ("CONC".equals(substructure.tag)) {
+			payload += substructure.payload;
+			return false;
+		} else {
+			sub.add(substructure);
+			substructure.sup = this;
+			return true;
+		}
+	}
+
+	/**
+	 * Given a map from xref_id strings to their parsed structures, populates the
+	 * <code>pointsTo</code> fields of this struct and its substructures. If
+	 * <code>cleanup</code> is <code>true</code>, also clears old xref_id payloads
+	 * and replaces dead pointers with <code"@VOID@"</code>.
+	 */
+	@SuppressWarnings({ "unchecked" })
+	public void convertPointers(final Map<String, GedStruct> xrefs, final boolean cleanup, final Class<?> cachetype) {
+		if (payload != null) {
+			final var to = xrefs.get(this.payload);
+
+			if (to != null) {
+				if (cachetype != null) {
+					try {
+						if (to.incoming == null)
+							to.incoming = (Collection<GedStruct>) cachetype.getConstructor().newInstance();
+
+						to.incoming.add(this);
+					} catch (Exception e) {
+						// intentionally left blank
+					}
+				}
+
+				pointsTo = to;
+
+				if (cleanup)
+					payload = null;
+			} else if (cleanup && FUZZY_XREF.matcher(payload).matches()) {
+				pointsTo = VOID;	// TODO: log this workaround
+			}
+		}
+
+		for (final var subStruct : sub)
+			subStruct.convertPointers(xrefs, cleanup, cachetype);
+	}
+
+	public void pointTo(final GedStruct struct) {
+		if (pointsTo == struct)
+			return;
+
+		if (pointsTo != null && pointsTo.incoming != null)
+			pointsTo.incoming.remove(this);
+
+		pointsTo = struct;
+
+		if (pointsTo != null) {
+			if (pointsTo.incoming == null)
+				pointsTo.incoming = new LinkedList<>();
+
+			pointsTo.incoming.add(this);
+		} else {
+			pointsTo = VOID;
+		}
+	}
+
+	/**
+	 * Serializes as GEDCOM. Advanced data overrides primitive data: for example,
+	 * superstructure supersedes level, pointers supersede payloads, etc.
+	 */
+	public String toString() {
+		final var sb = new StringBuilder();
+		serialize(sb);
+		return sb.toString();
+	}
+
+	void serialize(final StringBuilder sb) {
+		if (sup != null)
+			level = sup.level + 1;
+
+		sb.append(level);
+		sb.append(' ');
+
+		// TODO: add ID addition if no ID but pointed to
+		if (level == 0 && id != null) {
+			sb.append(id);
+			sb.append(' ');
+		}
+
+		if (uri == null && !"CONT".equals(tag) && !"TRLR".equals(tag))
+			sb.append("_EXT_");
+
+		sb.append(tag);
+
+		if (pointsTo != null) {
+			// TODO: add ID addition if point to non-ID struct
+			sb.append(' ');
+			sb.append(pointsTo.id);
+		} else if (payload != null) {
+			sb.append(' ');
+			sb.append(payload.replaceAll("^@|\\n@|\\r@", "$0@").replaceAll("\r\n?|\n", "\n" + (level + 1) + " CONT "));
+		}
+
+		//if (uri != null) sb.append("    <"+uri+">");
+
+		sb.append("\n");
+
+		for (final var subStruct : sub)
+			subStruct.serialize(sb);
+	}
 }
